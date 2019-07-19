89cdfd9c6476188a72234ad2a3ad84913aa960ef
dlh <948465697@qq.com>
2019-07-19 16:57:01 +0800

    qqq

---
+++ day04/src/main/java/User.java
@@ -0,0 +1,13 @@
+/**
+ * @ClassName User
+ * @Description
+ * @Author 不写代码的程序员
+ * @Date 16:53 2019/7/19
+ * @Version 2.1
+ **/
+
+public class User {
+    private String username;
+    private String age;
+
+}
